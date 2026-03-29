b5177251fe0d67db2cc0e4e14a7e2ce949a70d1e
sudhanshu kumar <sudhanshukumar0553@gmail.com>
2026-03-29 13:39:40 +0530

    fix

--- project-management-master/src/app/board/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { BoardScreen } from "@/components/BoardScreen";
-
-const DEFAULT_BOARD_ID = 1;
-
-export default function BoardPage() {
-  return <BoardScreen boardId={DEFAULT_BOARD_ID} />;
-}
